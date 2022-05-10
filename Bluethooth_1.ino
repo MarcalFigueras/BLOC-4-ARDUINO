419a5672df802ccdf9eabc36514dc6165b397cbc
Marsu <87938832+MarcalFigueras@users.noreply.github.com>
2022-05-10 16:34:58 +0200

    Add files via upload

---
+++ Bluethooth_1.ino
@@ -0,0 +1,38 @@
+int led_1 = 12;
+int led_2 = 13;
+
+char valor;
+
+void setup() {
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available())
+  {
+    valor=Serial.read();
+
+    if (valor =='A')
+    {
+      digitalWrite(led_1,HIGH);
+    }
+    
+    if (valor =='B')
+    {
+      digitalWrite(led_1,LOW);
+    }
+
+    if (valor =='C')
+    {
+      digitalWrite(led_2,HIGH);
+    }
+
+    if (valor =='D')
+    {
+      digitalWrite(led_2,LOW);
+    }
+  }
+  delay(100);
+}
